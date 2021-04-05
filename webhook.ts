f133cf7b61661015ae07b0ddf15097c4cfddf648
Passkou <passkou@163.com>
2021-04-06 02:02:45 +0800

    1

--- webhook.ts
+++ webhook.ts
@@ -1,14 +1,45 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
 import childProcess from 'child_process';
+import { promisify } from 'util';
 
 const app = new Koa();
+
+const exec = promisify(childProcess.exec);
 
 const SECRET = '069ece64193d5968c34b53acafc91a81';
 
 app.use(koaBody({
   parsedMethods: ['POST', 'GET', 'PUT', 'PATCH']
 }));
+
+async function procedure() {
+  console.log('拉取代码');
+  let o = await exec('git pull');
+  console.log(o.stdout);
+  if (o.stderr) {
+    console.error(o.stderr);
+    return;
+  }
+
+  console.log('构建镜像');
+  o = await exec('docker-compose build');
+
+  console.log(o.stdout);
+  if (o.stderr) {
+    console.error(o.stderr);
+    return;
+  }
+
+  console.log('启动服务');
+  o = await exec('docker-compose up -d');
+
+  console.log(o.stdout);
+  if (o.stderr) {
+    console.error(o.stderr);
+    return;
+  }
+}
 
 app.use(async (ctx, next) => {
   if (ctx.method !== 'GET') {
@@ -31,7 +62,7 @@ app.use(async (ctx, next) => {
 
   ctx.status = 202;
   ctx.body = '接受处理。';
-  childProcess.spawn('git pull && docker-compose build && docker-compose up -d');
+  procedure();
 });
 
 app.listen(5001, '127.0.0.1', undefined, () => {
