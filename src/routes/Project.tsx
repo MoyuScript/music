eb3980d0c08237f4dd6304ce26309957bda57d9c
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2023-08-09 21:41:44 +0800

    修复afdian样式

--- src/routes/Project.tsx
+++ src/routes/Project.tsx
@@ -117,7 +117,7 @@ const Project: React.FC<ProjectProps> = () => {
                                 frameBorder="0"
                                 className={
                                     clsx(
-                                        'w-full min700:w-[640px] h-[210px] p-0',
+                                        'w-full min700:w-[640px] h-[210px] p-0 shadow-xl rounded-xl',
                                         !isMobile() && 'translate-x-[6px]'
                                     )
                                 }
